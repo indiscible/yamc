aa1450ff630c57bf7f561243d87b7f513f2012b7
gilles <gillesp@gmail.com>
2015-12-19 20:08:57 +0100

    some test using the query module

---
+++ test.py
@@ -0,0 +1,4 @@
+k.Playlist.Add(item={ "file":'plugin://plugin.video.youtube/?action=play_video&videoid=B6jfrrwR10k' },playlistid=0)
+k.Player.Seek(playerid=0, value= {u'hours': 0, u'seconds': 22, u'minutes': 14})
+k.Player.GetProperties(playerid=0,properties=["duration"])
+k.Playlist.Add(item={ "file":'plugin://plugin.video.youtube/?action=play_video&videoid=B6jfrrwR10k' },playlistid=0)
